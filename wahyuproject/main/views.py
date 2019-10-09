ae71db8c6bf27cbc74f1fedf3ffdac4f0a789702
Norhashim <norhashim@Norhashims-MacBook-Pro.local>
2019-10-09 15:15:24 +0800

    can save orders now

--- wahyuproject/main/views.py
+++ wahyuproject/main/views.py
@@ -28,11 +28,171 @@ def order(request):
     dist = Distributors.objects.all
     orders = Orders.objects.all
 
-    if request.method == 'POST' :
-        name = request.POST.get("name")
-        contactno = request.POST.get("contactno")
-        email = request.POST.get("email")
+    if request.method == 'POST':
+        neworder = Orders()
+        neworder.orderno = "123456"
+        neworder.name = request.POST.get("name")
+        neworder.contactno = request.POST.get("contactno")
+        neworder.email = request.POST.get("email")
+        neworder.distributor = Distributors.objects.get(username=request.user)
+        try:
+            neworder.line1 = Products.objects.get(pk=int(request.POST.get("option1")))
+        except Products.DoesNotExist:
+            neworder.line1 = None
+        try:
+            neworder.line2 = Products.objects.get(pk=int(request.POST.get("option2")))
+        except Products.DoesNotExist:
+            neworder.line2 = None
+        try:
+            neworder.line3 = Products.objects.get(pk=int(request.POST.get('option3')))
+        except Products.DoesNotExist:
+            neworder.line3 = None
+        try:
+            neworder.line4 = Products.objects.get(pk=int(request.POST.get('option4')))
+        except Products.DoesNotExist:
+            neworder.line4 = None
+        try:
+            neworder.line5 = Products.objects.get(pk=int(request.POST.get('option5')))
+        except Products.DoesNotExist:
+            neworder.line5 = None
+        try:
+            neworder.line6 = Products.objects.get(pk=int(request.POST.get('option6')))
+        except Products.DoesNotExist:
+            neworder.line6 = None
+        try:
+            neworder.line7 = Products.objects.get(pk=int(request.POST.get('option7')))
+        except Products.DoesNotExist:
+            neworder.line7 = None
+        try:
+            neworder.line8 = Products.objects.get(pk=int(request.POST.get('option8')))
+        except Products.DoesNotExist:
+            neworder.line8 = None
+        try:
+            neworder.line9 = Products.objects.get(pk=int(request.POST.get('option9')))
+        except Products.DoesNotExist:
+            neworder.line9 = None
+        try:
+            neworder.line10 = Products.objects.get(pk=int(request.POST.get('option10')))
+        except Products.DoesNotExist:
+            neworder.line10 = None 
+        try:      
+            neworder.line11 = Products.objects.get(pk=int(request.POST.get('option11')))
+        except Products.DoesNotExist:
+            neworder.line11 = None
+        try:
+            neworder.line12 = Products.objects.get(pk=int(request.POST.get('option12')))
+        except Products.DoesNotExist:
+            neworder.line12 = None
+        try:
+            neworder.line13 = Products.objects.get(pk=int(request.POST.get('option13')))
+        except Products.DoesNotExist:
+            neworder.line13 = None
+        try:
+            neworder.line14 = Products.objects.get(pk=int(request.POST.get('option14')))
+        except Products.DoesNotExist:
+            neworder.line14 = None
+        try:
+            neworder.line15 = Products.objects.get(pk=int(request.POST.get('option15')))
+        except Products.DoesNotExist:
+            neworder.line15 = None
+        try:
+            neworder.line16 = Products.objects.get(pk=int(request.POST.get('option16')))
+        except Products.DoesNotExist:
+            neworder.line16 = None
+        try:
+            neworder.line17 = Products.objects.get(pk=int(request.POST.get('option17')))
+        except Products.DoesNotExist:
+            neworder.line17 = None
+        try:
+            neworder.line18 = Products.objects.get(pk=int(request.POST.get('option18')))
+        except Products.DoesNotExist:
+            neworder.line18 = None
+        try:
+            neworder.line19 = Products.objects.get(pk=int(request.POST.get('option19')))
+        except Products.DoesNotExist:
+            neworder.line19 = None
+        try:
+            neworder.line20 = Products.objects.get(pk=int(request.POST.get('option20')))
+        except Products.DoesNotExist:
+            neworder.line20 = None
+        try:
+            neworder.line21 = Products.objects.get(pk=int(request.POST.get('option21')))
+        except Products.DoesNotExist:
+            neworder.line21 = None
+        try:
+            neworder.line22 = Products.objects.get(pk=int(request.POST.get('option22')))
+        except Products.DoesNotExist:
+            neworder.line22 = None
+        try:
+            neworder.line23 = Products.objects.get(pk=int(request.POST.get('option23')))
+        except Products.DoesNotExist:
+            neworder.line23 = None
+        try:
+            neworder.line24 = Products.objects.get(pk=int(request.POST.get('option24')))
+        except Products.DoesNotExist:
+            neworder.line24 = None
+        try:
+            neworder.line25 = Products.objects.get(pk=int(request.POST.get('option25')))
+        except Products.DoesNotExist:
+            neworder.line25 = None
+        try:
+            neworder.line26 = Products.objects.get(pk=int(request.POST.get('option26')))
+        except Products.DoesNotExist:
+            neworder.line26 = None
+        try:
+            neworder.line27 = Products.objects.get(pk=int(request.POST.get('option27')))
+        except Products.DoesNotExist:
+            neworder.line27 = None
+        try:
+            neworder.line28 = Products.objects.get(pk=int(request.POST.get('option28')))
+        except Products.DoesNotExist:
+            neworder.line28 = None
+        try:
+            neworder.line29 = Products.objects.get(pk=int(request.POST.get('option29')))
+        except Products.DoesNotExist:
+            neworder.line29 = None
+        try:
+            neworder.line30 = Products.objects.get(pk=int(request.POST.get('option30')))
+        except Products.DoesNotExist:
+            neworder.line30 = None
         
+
+        neworder.qty1 = request.POST.get('qty1')
+        neworder.qty2 = request.POST.get('qty2')
+        neworder.qty3 = request.POST.get('qty3')
+        neworder.qty4 = request.POST.get('qty4')
+        neworder.qty5 = request.POST.get('qty5')
+        neworder.qty6 = request.POST.get('qty6')
+        neworder.qty7 = request.POST.get('qty7')
+        neworder.qty8 = request.POST.get('qty8')
+        neworder.qty9 = request.POST.get('qty9')
+        neworder.qty10 = request.POST.get('qty10')
+        neworder.qty11 = request.POST.get('qty11')
+        neworder.qty12 = request.POST.get('qty12')
+        neworder.qty13 = request.POST.get('qty13')
+        neworder.qty14 = request.POST.get('qty14')
+        neworder.qty15 = request.POST.get('qty15')
+        neworder.qty16 = request.POST.get('qty16')
+        neworder.qty17 = request.POST.get('qty17')
+        neworder.qty18 = request.POST.get('qty18')
+        neworder.qty19 = request.POST.get('qty19')
+        neworder.qty20 = request.POST.get('qty20')
+        neworder.qty21 = request.POST.get('qty21')
+        neworder.qty22 = request.POST.get('qty22')
+        neworder.qty23 = request.POST.get('qty23')
+        neworder.qty24 = request.POST.get('qty24')
+        neworder.qty25 = request.POST.get('qty25')
+        neworder.qty26 = request.POST.get('qty26')
+        neworder.qty27 = request.POST.get('qty27')
+        neworder.qty28 = request.POST.get('qty28')
+        neworder.qty29 = request.POST.get('qty29')
+        neworder.qty30 = request.POST.get('qty30')
+
+        neworder.address = request.POST.get('address')
+        neworder.prefferedtiming = request.POST.get("prefferedtime",None)
+        neworder.preffereddate = request.POST.get("deliverydate")
+
+        neworder.save()
 
         return redirect('index')
     else:
@@ -42,7 +202,7 @@ def order(request):
             'products' : products,
             'dist' : dist,
             'orders' : orders,
-            'total_lineItems' : range(1,11)
+            'total_lineItems' : range(1,31)
         }
         return render(request,'main/order.html', content)
     
